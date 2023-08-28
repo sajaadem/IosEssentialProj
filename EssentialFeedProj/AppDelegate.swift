ff0f61006b73dc6a8b6782243b24869a1c83c191
sajaadem <sajaadem@gmail.com>
2023-08-29 01:27:12 +0300

    first commit

---
+++ EssentialFeedProj/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  EssentialFeedProj
+//
+//  Created by saja adem on 29/08/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ EssentialFeedProjTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,11 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  EssentialFeedProjUITests
+//
+//  Created by saja adem on 29/08/2023.
+//
+
+import XCTest
+
+class EssentialFeedProjTests: XCTestCase {
+}
